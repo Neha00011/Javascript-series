0c17a8efce176111bcef8dcc96073248768c7a4e
Neha Panda <112271756+Neha00011@users.noreply.github.com>
2023-09-25 18:19:30 +0000

    conversion of datatypes

---
+++ 01_basics/01_conversionOperation.js
@@ -0,0 +1,32 @@
+let score = "33"
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+console.log(typeof(valueInNumber));
+
+console.log(valueInNumber);
+
+//for null result=0
+//for undefined or 33abc result=NAN
+//for true result=1
+//for string result=NAN
+//for false result=0
+//for 33 result=33
+
+let loggedIn =1
+let booleanloggedIn= Boolean(loggedIn)
+console.log(booleanloggedIn);
+
+//if 1 then true
+//if 0 then false
+//if empty string then false
+//if valid string then true
+
+let someNumber  =33
+let stringNumber =String(someNumber)
+console.log(stringNumber);
+console.log(typeof(stringNumber));
+
