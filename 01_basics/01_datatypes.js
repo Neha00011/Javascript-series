92d4ab3039a3b4ab43d86bc494f1729e971f756a
Neha Panda <112271756+Neha00011@users.noreply.github.com>
2023-09-25 18:03:34 +0000

    datatypes in js

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,25 @@
+"use strict" ; //treat all js code as newer version
+
+//alert(3+2) //we are using nodejs,not browser
+
+console.log(3+3);
+console.log("neha");//code readability should be high
+
+let name = "neha"
+let age=21
+let isLoggedIn =false
+
+
+//number =>2 to power 53
+//bigint
+//string=>" "
+//boolean=true/false
+//null is a stand alone value
+//undefined =>not a representation of undefined value
+//symbol=>uniqueness
+
+
+//object
+//console.log(typeof null) = object
+//console.log(tyoeof undefined) = undefined
+console.log(typeof age);
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 1234
+let accountEmail = "neha17@google.com"
+//var accountPassword = "12345"
+let accountPassword = "12345"
+/*avoid use of var because of issue of block scope and functional scope
+*/
+accountCity = "Jaipur"
+let accountState;
+//undefined
+
+//accountId = 2
+accountEmail = "sikta@google.com"
+accountPassword = "88739"
+accountCity = "kolkata"
+ console.log(accountEmail);
+ console.log(accountPassword);
+ console.log(accountCity);
+
+ console.table([accountEmail,accountPassword,accountCity],accountState);
